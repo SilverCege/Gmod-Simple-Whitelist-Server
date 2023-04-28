a2ed2232efc20c5fd3fe52733c640ef403f566c1
SilverCege <erwangenay@gmail.com>
2023-04-28 05:38:16 +0200

    Update whitelist.lua

--- lua/autorun/server/whitelist.lua
+++ lua/autorun/server/whitelist.lua
@@ -1,16 +1,15 @@
-whitelist = false -- true to turn on and false to turn off the system
+whitelist = true -- true to turn on and false to turn off the system
 allowed = {
-	[ "76561198012345678" ] = true, -- Me
-	[ "76561198123456789" ] = true, -- Friend #1
-	[ "76561198234567890" ] = true, -- Friend #2
-	[ "76561198242989986" ] = false, -- Papi Caneton
+	[ "76561198012345678" ] = true, -- Me ==> can connect
+	[ "76561198123456789" ] = true, -- Friend #1 ==> can connect
+	[ "76561198234567890" ] = false, -- Friend #2 ==> can't connect
 }
 
-hook.Add( "PlayerInitialSpawn", "access_whitelistppp", function(ply)
+hook.Add( "PlayerInitialSpawn", "access_whitelist_player_connection", function(ply)
 	local steamID = ply:SteamID64()
 	if whitelist == true then
 	if not allowed[ steamID ] then
-		ply:Kick("Tu n'es pas whitelist\n\n            discord.gg/HqeQSaUgcB")
+		ply:Kick("You are not whitelisted")
 	end
 	end
 end )
